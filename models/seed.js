f1f03f5a9b924f816a7dc732109f54088cd9b5bb
Allan Wazacz <cazwazacz@gmail.com>
2018-01-10 11:30:37 +0000

    Drop database before seeding

--- models/seed.js
+++ models/seed.js
@@ -1,78 +1,61 @@
 const Question = require('./question');
 const Quiz = require('./quiz');
+const mongoose = require('mongoose');
 
-var questions = [
-  new Question({
+const conn = mongoose.connect('mongodb://localhost:27017/pub-quiz-development');
+mongoose.connection.on('open', function() {
+
+  conn.connection.db.dropDatabase();
+
+  const questionFrance = new Question({
     type: 'MultipleChoice',
     text: 'What is the capital of the France',
     options: ['Rome', 'London', 'Paris'],
     answer: [2]
-  }),
+  });
 
-  new Question({
+  const questionSky = new Question({
     type: 'MultipleChoice',
     text: 'What is the color of the sky?',
-    options: ['Purple', 'Blue', 'Green', 'Yellow'],
+    options: ['Purple', 'Blue', 'Green'],
     answer: [1]
-  }),
+  });
 
-  new Question({
-    type: 'MultipleChoice',
-    text: 'What is Allan\'s favourite band?',
-    options: ['Spice Girls', 'Oasis', 'One Direction'],
-    answer: [2]
-  }),
 
-  new Question({
-    type: 'MultipleChoice',
-    text: 'What is Theo\s favourite food?',
-    options: ['Marmite', 'Hummous', 'Turnips'],
-    answer: [2]
-  }),
+  const multipleChoiceQuiz =  new Quiz({name : 'Multiple Choice Quiz'}); // Document(data in the collection/table)
+  multipleChoiceQuiz.questions.push(questionFrance)
+  multipleChoiceQuiz.questions.push(questionSky)
 
-  new Question({
-    type: 'MultipleChoice',
-    text: 'How many names has Kay had in her entire lifetime?',
-    options: ['5', '6', '7'],
+  multipleChoiceQuiz.save(function (err, quiz) {
+    if (err) {
+      console.error('Could not save');
+    };
+  });
+
+  const questionPingPong = new Question({
+    type: 'Text',
+    text: 'What do Oleg and Tabish love to play?',
+    options: ['Ping Pong'],
     answer: [0]
-  })
-]
+  });
+
+  const questionRob = new Question({
+    type: 'Text',
+    text: "What is Rob's nickname?",
+    options: ['Sneaky Rob'],
+    answer: [0]
+  });
+
+  const textQuiz = new Quiz({name : 'Text Quiz'});
 
 
-const multipleChoiceQuiz =  new Quiz({ name : 'Multiple Choice Quiz' }); // Document(data in the collection/table)
+  textQuiz.questions.push(questionPingPong)
+  textQuiz.questions.push(questionRob)
 
-questions.forEach(function(question) {
-  multipleChoiceQuiz.questions.push(question);
-});
+  textQuiz.save(function (err, quiz) {
+    if (err) {
+      console.error('Could not save');
+    };
+  });
 
-multipleChoiceQuiz.save(function (err, quiz) {
-  if (err) {
-    console.error('Could not save');
-  };
-});
-
-const questionPingPong = new Question({
-  type: 'Text',
-  text: 'What do Oleg and Tabish love to play?',
-  options: ['Ping Pong'],
-  answer: [0]
-});
-
-const questionRob = new Question({
-  type: 'Text',
-  text: "What is Rob's nickname?",
-  options: ['Sneaky Rob'],
-  answer: [0]
-});
-
-const textQuiz = new Quiz({name : 'Text Quiz'});
-
-
-textQuiz.questions.push(questionPingPong)
-textQuiz.questions.push(questionRob)
-
-textQuiz.save(function (err, quiz) {
-  if (err) {
-    console.error('Could not save');
-  };
-});
+})
