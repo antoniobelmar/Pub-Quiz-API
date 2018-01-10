4f4afce47cc932799fe359cfcf222605b69303da
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-10 17:18:12 +0000

    Add tests that test if json error message is sent, json message is sent. Also tests if findById is called.

--- test/unit/quizControllerTest.js
+++ test/unit/quizControllerTest.js
@@ -1,15 +1,42 @@
-import {expect} from 'chai';
-import {quizController} from 'controllers/quizController';
-import {spy, stub} from 'sinon';
+const expect = require('chai').expect;
+const quizController = require('../../controllers/quizController');
+const sinon = require('sinon');
 
 describe('quizController', function(){
-  const request, res, quizModel
+  var request, res, quizModel;
 
   beforeEach(function(){
-    request = {params: {id: 7}}
-    quizModel = {
-      findById: spy();
-    }
+    request = { params: { id: 7 } };
+    quizModel = {};
+    res = { json: sinon.spy() };
+  });
 
+  describe('when error is raised', function() {
+    let error;
+
+    beforeEach(function(){
+      quizModel.findById = function(id, callback) {
+        callback(true);
+      };
+      error = { error: "Not Found" };
+      quizController(request, res, null, quizModel);
+    });
+
+    it('should send json error message', function() {
+      expect(res.json.calledWith(error)).to.be.true;
+    });
+  });
+
+  describe('when quiz is found', function() {
+    beforeEach(function() {
+      quizModel.findById = function(id, callback) {
+        callback(false, 5);
+      };
+      quizController(request, res, null, quizModel);
+    });
+
+    it('should send json', function() {
+      expect(res.json.calledWith(5)).to.be.true;
+    });
   });
 });
