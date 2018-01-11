c754a90a9a5c40a8fcaeb4ea4ff8745a7d08dd22
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-01-11 18:12:28 +0000

    Stop integration tests from hanging

--- test/integration/apiTest.js
+++ test/integration/apiTest.js
@@ -1,39 +1,44 @@
+const mongoose = require('mongoose');
 const supertest = require('supertest');
-const assert = require('assert');
-const app = require('../../index');
+const app = require('../../app');
 const Quiz = require('../../models/quiz');
+const assert = require('assert');
 
 describe('api route', function() {
+  after(function() {
+    mongoose.disconnect();
+  });
+
   it('should return Ok', function(done) {
     supertest(app)
-    .get('/api/quiz/1')
-    .expect(200)
-    .end(done);
-  })
+    .get('/quiz/1')
+    .expect(200, done);
+  });
 
   it('should say not found is quiz does not exist', function(done) {
     supertest(app)
-    .get('/api/quiz/1')
-    .expect(200)
-    .end(function(err, response) {
-      assert.ok(JSON.stringify(response.body) === '{"error":"Not Found"}')
-      return done();
-    })
-  })
+      .get('/quiz/1')
+      .expect(200)
+      .end(function(err, response) {
+        assert.ok(JSON.stringify(response.body) === '{"error":"Not Found"}') 
+        return done();
+      });
+  });
 
   it('should return quiz if it exists', function(done) {
     Quiz.findOne({}, function(err, quiz) {
       if (err) {
-        console.log(err);
-      } else {
-        supertest(app)
-        .get('/api/quiz/' + quiz.id)
+        throw err
+      };
+      supertest(app)
+        .get('/quiz/' + quiz.id)
         .expect(200)
         .end(function(err, response) {
           assert.ok(JSON.stringify(response.body) === JSON.stringify(quiz));
           return done();
-        })
-      }
-    })
-  })
-})
+        });
+    });
+  });
+});
+
+
