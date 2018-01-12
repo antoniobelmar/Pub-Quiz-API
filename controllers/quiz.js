42c69b1cfefd5e418580bcaa7cb646c2edefdccc
Allan Wazacz <cazwazacz@gmail.com>
2018-01-12 12:49:42 +0000

    Add index route for quiz controller

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -3,6 +3,9 @@ const getQuiz = require('./quizActions/getQuiz');
 
 const router = express.Router();
 
+router.get('/',  function(req, res) {
+  res.send('1231231');
+});
 router.get('/:id', getQuiz);
 
 module.exports = router;
