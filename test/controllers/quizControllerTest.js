f0df06cd2d1fc8c208d8cc47b4037ea5b1e5f986
Allan Wazacz <cazwazacz@gmail.com>
2018-01-12 13:03:08 +0000

    Add tests for  getAllQuizzes method

--- test/controllers/quizControllerTest.js
+++ test/controllers/quizControllerTest.js
@@ -1,8 +1,9 @@
 const expect = require('chai').expect;
 const getQuiz = require('../../controllers/quizActions/getQuiz');
+const getAllQuizzes = require('../../controllers/quizActions/getAllQuizzes');
 const sinon = require('sinon');
 
-describe('getQuiz', function(){
+describe('quizController', function() {
   var request, res, quizModel;
 
   beforeEach(function(){
@@ -11,32 +12,65 @@ describe('getQuiz', function(){
     res = { json: sinon.spy() };
   });
 
-  describe('when error is raised', function() {
-    let error;
+  describe('getQuiz', function(){
+    describe('when error is raised', function() {
+      let error;
 
-    beforeEach(function(){
-      quizModel.findById = function(id, callback) {
-        callback(true);
-      };
-      error = { error: "Not Found" };
-      getQuiz(request, res, null, quizModel);
+      beforeEach(function(){
+        quizModel.findById = function(id, callback) {
+          callback(true);
+        };
+        error = { error: "Not Found" };
+        getQuiz(request, res, null, quizModel);
+      });
+
+      it('should send json error message', function() {
+        expect(res.json.calledWith(error)).to.be.true;
+      });
     });
 
-    it('should send json error message', function() {
-      expect(res.json.calledWith(error)).to.be.true;
+    describe('when quiz is found', function() {
+      beforeEach(function() {
+        quizModel.findById = function(id, callback) {
+          callback(false, 5);
+        };
+        getQuiz(request, res, null, quizModel);
+      });
+
+      it('should send json', function() {
+        expect(res.json.calledWith(5)).to.be.true;
+      });
     });
   });
 
-  describe('when quiz is found', function() {
-    beforeEach(function() {
-      quizModel.findById = function(id, callback) {
-        callback(false, 5);
-      };
-      getQuiz(request, res, null, quizModel);
+  describe('getAllQuizzes', function() {
+    describe('when error is raised', function() {
+      let error;
+
+      beforeEach(function(){
+        quizModel.find = function(query, callback) {
+          callback(true);
+        };
+        error = { error: "Not Found" };
+        getAllQuizzes(request, res, null, quizModel);
+      });
+
+      it('should send json error message', function() {
+        expect(res.json.calledWith(error)).to.be.true;
+      });
     });
 
-    it('should send json', function() {
-      expect(res.json.calledWith(5)).to.be.true;
+    describe('when quiz is found', function() {
+      beforeEach(function() {
+        quizModel.find = function(query, callback) {
+          callback(false, 5);
+        };
+        getAllQuizzes(request, res, null, quizModel);
+      });
+
+      it('should send json', function() {
+        expect(res.json.calledWith(5)).to.be.true;
+      });
     });
-  });
-});
+  })
+})
