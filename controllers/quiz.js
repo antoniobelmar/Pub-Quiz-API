7a55c5212d160686d575f2d47d76d1060b801c1c
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-14 15:33:07 +0000

    Add post route

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const getAllQuizzes = require('./quizActions/getAllQuizzes');
 const getQuiz = require('./quizActions/getQuiz');
+const createQuiz = require('./quizActions/createQuiz');
+
 
 const router = express.Router();
 
 router.get('/', getAllQuizzes);
 router.get('/:id', getQuiz);
+router.post('/', createQuiz);
 
 module.exports = router;
