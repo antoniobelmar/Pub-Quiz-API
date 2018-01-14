f94815dcff017149f52e6c7593b108799888e6fa
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-14 16:03:49 +0000

    createQuiz saves questions in database

--- controllers/quizActions/createQuiz.js
+++ controllers/quizActions/createQuiz.js
@@ -1,7 +1,7 @@
 const Quiz = require('../../models/quiz');
 
-function createQuiz(req, res, quizModel = Quiz ){
-  var quiz = new Quiz({name: req.body.QuizName})
+function createQuiz(req, res, quizModel = Quiz ) {
+  var quiz = new Quiz({name: req.body.name, questions: req.body.questions})
   quiz.save(function (err, quiz) {
     if (err) {
       console.error('Could not save');
