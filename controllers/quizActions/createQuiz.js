b3624ab9b56cb44ed5620ca96de868e73b136f8d
Allan Wazacz <cazwazacz@gmail.com>
2018-01-14 22:36:06 +0000

    Use Question and Option mongoose models to save questions of quizzes

--- controllers/quizActions/createQuiz.js
+++ controllers/quizActions/createQuiz.js
@@ -1,7 +1,22 @@
 const Quiz = require('../../models/quiz');
+const Question = require('../../models/question');
+const Option = require('../../models/option');
 
 function createQuiz(req, res, quizModel = Quiz ) {
-  var quiz = new Quiz({name: req.body.name, questions: req.body.questions})
+  let questionsArray = [];
+  req.body.questions.forEach(function(question) {
+    let optionsArray = [];
+    question._options.forEach(function(option) {
+      optionsArray.push(new Option({ text: option._text }))
+    });
+    questionsArray.push(new Question({
+      type: question._type,
+      text: question._text,
+      options: optionsArray,
+      answer: question._answer
+    }))
+  });
+  let quiz = new Quiz({name: req.body.name, questions: questionsArray})
   quiz.save(function (err, quiz) {
     if (err) {
       console.error('Could not save');
