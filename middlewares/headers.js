9656e925b98a4694d5a852ccc756d7be1d1e1914
Allan Wazacz <cazwazacz@gmail.com>
2018-01-15 12:13:29 +0000

    Remove cors library and comments

--- middlewares/headers.js
+++ middlewares/headers.js
@@ -1,25 +1,13 @@
 'use strict';
 
 function headers(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
-  // next();
-
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
   //intercepts OPTIONS method
   if ('OPTIONS' === req.method) {
-    //respond with 200
-    console.log('-------------------');
-    console.log('AT OPTIONS REQUEST');
     var headers = {};
-    // IE8 does not allow domains to be specified, just the *
-    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
     headers["Access-Control-Allow-Origin"] = "*";
     headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
     headers["Access-Control-Allow-Credentials"] = false;
@@ -27,9 +15,7 @@ function headers(req, res, next) {
     headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
     res.writeHead(200, headers);
     res.end();
-
-  }
-  else {
+  } else {
   //move on
     next();
   }
