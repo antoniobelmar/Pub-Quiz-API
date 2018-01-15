91822a9cbbfa34c5570ce9846ce30d73aee996f7
Allan Wazacz <cazwazacz@gmail.com>
2018-01-15 11:56:13 +0000

    Send response headers when options request is made

--- middlewares/headers.js
+++ middlewares/headers.js
@@ -17,8 +17,17 @@ function headers(req, res, next) {
     //respond with 200
     console.log('-------------------');
     console.log('AT OPTIONS REQUEST');
-    next();
-    // res.send(200);
+    var headers = {};
+    // IE8 does not allow domains to be specified, just the *
+    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = false;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+    res.writeHead(200, headers);
+    res.end();
+
   }
   else {
   //move on
