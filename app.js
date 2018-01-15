b99d72bc2d5f722f9b0ef1b57b81c1900da46a85
Allan Wazacz <cazwazacz@gmail.com>
2018-01-15 12:12:25 +0000

    Remove comments which were using cors library

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const db = require('./db');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
 
 // middlewares
 
 app.use(require('./middlewares/headers'));
-// app.use(cors());
-// app.options('*', cors());
 app.use(bodyParser.json());
 
 // routes
