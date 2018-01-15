6a9cc4229f18bf6fe2a98fb11c6755edb5a2cbc5
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-01-15 16:28:52 +0000

    Include ws-express in app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const db = require('./db');
 const bodyParser = require('body-parser');
 
+// app and web sockets
+
 const app = express();
+const expressWs = require('express-ws')(app);
 
 // middlewares
 
@@ -10,7 +13,6 @@ app.use(require('./middlewares/headers'));
 app.use(bodyParser.json());
 
 // routes
-
 
 app.use('/quiz', require('./controllers/quiz'));
 
