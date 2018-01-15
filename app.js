fcc7650e82e7353187ba833398930277df83312c
Allan Wazacz <cazwazacz@gmail.com>
2018-01-15 11:22:05 +0000

    Use cors library

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const db = require('./db');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 // middlewares
 
-app.use(require('./middlewares/headers'));
+// app.use(require('./middlewares/headers'));
+app.use(cors());
 app.use(bodyParser.json());
 
 // routes
+
 
 app.use('/quiz', require('./controllers/quiz'));
 
