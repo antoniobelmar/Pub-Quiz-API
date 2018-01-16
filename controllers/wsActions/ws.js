6fbb4b94723e4390ac8be8792bf4e144f5e3a6e3
antonio <antoniobelmardacosta@gmail.com>
2018-01-16 17:59:15 +0000

    Call finishQuiz method from Game model once kill message is received

--- controllers/wsActions/ws.js
+++ controllers/wsActions/ws.js
@@ -1,9 +1,9 @@
 'use strict';
-
+const Game = require('../../models/game')
 const partiesModule = require('./helpers/parties');
 let parties = new partiesModule.Parties();
 
-function getOnMessage(ws, req) {
+function getOnMessage(ws, req, model = Game) {
   return function onMessage(message) {
     console.log('got message', message);
     let party = parties.get(req.url);
@@ -27,13 +27,14 @@ function getOnMessage(ws, req) {
       case 'kill':
         parties.remove(party);
         party.kill()
+        model.finishQuiz(data.wsId)
       break;
     };
   };
 };
 
 function wsConnection(ws, req) {
-  console.log('recieved request');
+  console.log('received request');
   let party = parties.get(req.url);
   party.addPlayer(ws);
   ws.on('message', getOnMessage(ws, req));
